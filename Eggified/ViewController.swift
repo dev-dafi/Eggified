eaacf8680af442f001b8936965c3b9d771f0a336
Daniel Fischer <dev_dafi@icloud.com>
2020-06-13 16:47:22 +0200

    Initial Commit

---
+++ Eggified/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eggified
+//
+//  Created by Daniel Fischer on 13.06.20.
+//  Copyright © 2020 Daniel Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
