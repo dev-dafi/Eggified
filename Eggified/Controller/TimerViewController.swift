282f1da93b766070bc62b3bb58dee25f619320ca
Daniel Fischer <dev_dafi@icloud.com>
2020-06-16 15:48:52 +0200

    Creating new UILabel and constraints. adding setup and format functions and add a function to determine, which egg button was pressed.

--- Eggified/Controller/TimerViewController.swift
+++ Eggified/Controller/TimerViewController.swift
@@ -9,18 +9,72 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
     @IBOutlet weak var timerLabel: UILabel!
     
-    
+    @IBOutlet weak var boilButton: UIButton!
+    @IBOutlet weak var dontBoilButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
- 
-      
+        setStartUpConditions()
+        
+        
     }
-
-   
+    /**
+     Determines the pressed button and set the timerLabel to the appropriate time. The function also enables the boilButton,
+     so that the user coud start the timer.
+     */
+    @IBAction func eggButtonPressed(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 1:
+            timerLabel.text = timeFormatted(300)
+            boilButton.isEnabled = true
+        case 2:
+            timerLabel.text = timeFormatted(420)
+            boilButton.isEnabled = true
+        case 3:
+            timerLabel.text = timeFormatted(600)
+            boilButton.isEnabled = true
+        default:
+            timerLabel.text = "0"
+        }
+    }
+    
+    
+    
+    
+    //MARK: - Setup And Format Functions
+    
+    /**
+     Formats the given time in minutes and seconds.
+     
+     - Parameter totalSeconds: Via egg button selected time in seconds
+     - Returns: A formated string for the timerLabel
+     */
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        //     let hours: Int = totalSeconds / 3600
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    /**
+     Sets properties for boil buttons and timeLabel.
+     */
+    func setStartUpConditions(){
+        
+        // Button configuration
+        dontBoilButton.isHidden = true
+        boilButton.isEnabled = false
+        
+        //label configuration
+        timerLabel.adjustsFontSizeToFitWidth=true;
+        timerLabel.text = "Select your egg"
+    }
+    
+    
 }
 
